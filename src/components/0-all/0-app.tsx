b66c9fd85769d6ee012f71c6ee34bd89a3841a9b
Max Zakharzhevskiy <maxzz@msn.com>
2026-09-04 20:18:10 -0700

    WIP. main-section-layout. add debug screens

--- src/components/0-all/0-app.tsx
+++ src/components/0-all/0-app.tsx
@@ -9,7 +9,7 @@ export function App() {
         <Toaster />
         <AllDialogs />
         
-        <main className="h-dvh text-xs bg-background overflow-hidden grid grid-rows-[auto_1fr_auto]">
+        <main className="h-dvh text-xs bg-background overflow-hidden grid grid-rows-[auto_1fr_auto] debug-screens">
             <Header />
             <MainBody />
             <Section3_Footer />
