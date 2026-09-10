09c8505f07f0c73e942c6177dec3114ddb321aa1
Max Zakharzhevskiy <maxzz@msn.com>
2026-09-09 20:57:48 -0700

    WIP. update header. 4

--- src/components/0-all/0-app.tsx
+++ src/components/0-all/0-app.tsx
@@ -9,7 +9,7 @@ export function App() {
         <Toaster />
         <AllDialogs />
         
-        <main className="h-dvh text-xs bg-background overflow-hidden grid grid-rows-[auto_1fr_auto] debug-screens">
+        <main className="h-dvh text-xs bg-background overflow-hidden grid grid-rows-[auto_1fr_auto]">
             <Header />
             <MainBody />
             <Section3_Footer />
