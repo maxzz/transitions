d24ed6369fb32f8d214ff55d440805124862c825
Max Zakharzhevskiy <maxzz@msn.com>
2026-09-09 21:23:54 -0700

    WIP. hero-page. code review

--- src/components/0-all/0-app.tsx
+++ src/components/0-all/0-app.tsx
@@ -13,16 +13,19 @@ export function App() {
         <Toaster />
         <AllDialogs />
 
-        {currentPage === "hero" ? (
-            <main className="h-dvh text-xs bg-background overflow-hidden">
-                <HeroPage />
-            </main>
-        ) : (
-            <main className="h-dvh text-xs bg-background overflow-hidden grid grid-rows-[auto_1fr_auto]">
-                <Header />
-                <MainBody />
-                <Section3_Footer />
-            </main>
-        )}
+        {currentPage === "hero"
+            ? (
+                <main className="h-dvh text-xs bg-background overflow-hidden">
+                    <HeroPage />
+                </main>
+            )
+            : (
+                <main className="h-dvh text-xs bg-background overflow-hidden grid grid-rows-[auto_1fr_auto]">
+                    <Header />
+                    <MainBody />
+                    <Section3_Footer />
+                </main>
+            )
+        }
     </>);
 }
